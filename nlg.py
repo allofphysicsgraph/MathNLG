c276da000d9f727a7136649053b8ee0beb456fa1
marekyggdrasil <marek.yggdrasil@gmail.com>
2018-07-08 18:30:21 +0800

    project template with readme and licence

---
+++ nlg.py
@@ -0,0 +1,21 @@
+
+# accepts knowledge source ks
+# returns document plan dp
+def documentPlanner(ks) :
+    dp = None
+    # TODO
+    return dp
+
+# accepts document plan dp
+# returns text specification ts
+def microplanner(dp) :
+    ts = None
+    # TODO
+    return ts
+
+# accepts text specification ts
+# returns surface text st
+def surfaceRealiser(ts) :
+    st = None
+    # TODO
+    return st
---
+++ run.py
@@ -0,0 +1,22 @@
+from lxml import etree
+from sympy import Symbol, sympify
+
+from nlg import documentPlanner, microplanner, surfaceRealiser
+
+# get knowledge source from the source.xml file
+ks = None
+with open('source.xml', 'r') as file :
+    xml = file.read()
+    ks = etree.XML(xml)
+
+# get document plan from the knowledge source
+dp = documentPlanner(ks)
+
+# get text specification from microplanner
+ts = microplanner(dp)
+
+# get surface text from surface realiser
+st = surfaceRealiser(ts)
+
+# print the output
+print st
