ba45bb30203e4bccadcd2ee70d6d72a6f52e58b0
marekyggdrasil <marek.yggdrasil@gmail.com>
2018-08-15 10:22:46 +0200

    styling, table of contents, anchors

--- generate.py
+++ generate.py
@@ -31,17 +31,18 @@ for id in data['source'].keys() :
     else :
         title = (linguistic['linguistic'][id]['noun']).capitalize()
     content = surfaceRealize(entry, meta)
-    output.append({'title': title, 'content': content})
+    output.append({'title': title, 'id': id, 'content': content})
 
 txt = ''
+toc = '<ul class="toc_list">'
 for entry in output :
-    txt += '<h3>%s</h3><p>%s</p>' % (entry['title'], entry['content'])
-
-#print txt
+    txt += '<h3 id="%s" class="nlg">%s</h3><p>%s</p><a href="#toc_container">Go to top</a><hr>' % (entry['id'], entry['title'], entry['content'])
+    toc += '<li><a href="#%s">%s</a></li>' % (entry['id'], entry['title'])
+toc += '</ul>'
 
 html = ''
 with open('templates/index.html', 'r') as file :
-    html = Template(file.read()).render({'content': txt})
+    html = Template(file.read()).render({'toc': toc, 'content': txt})
 
 with open('docs/index.html', 'w') as file:
     file.write(html)
