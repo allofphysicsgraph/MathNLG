bd09fd1102682cdebf89710a0029d49779a7098a
marekyggdrasil <marek.yggdrasil@gmail.com>
2018-08-16 21:57:43 +0200

    implemented document planning

--- generate.py
+++ generate.py
@@ -3,6 +3,22 @@ import json
 from jinja2 import Template
 from nlg.surface import surfaceRealize
 
+def menu(node, rendered, linguistic) :
+    if 'surface' in node.keys() :
+        id = node['surface']
+        entry = rendered[id]
+        return '<li><a href="#%s">%s</a></li>' % (entry['id'], entry['title'])
+    if 'title' in node.keys() :
+        id = node['title']
+        all = linguistic[id]['noun'].capitalize()
+        if 'children' in node.keys() :
+            compiled = ''
+            for child in node['children'] :
+                build = menu(child, rendered, linguistic)
+                compiled += build
+            all += '<ul class="toc_list">%s</ul>' % (compiled)
+        return '<li>%s</li>' % (all)
+
 def parse(text) :
     try:
         return json.loads(text)
@@ -19,26 +35,33 @@ with open('data/linguistic.json', 'r') as file :
     raw = file.read()
     linguistic = parse(raw)
 
-output = []
+planner = {}
+with open('data/plan.json', 'r') as file :
+    raw = file.read()
+    planner = parse(raw)
+
+rendered = {}
 for id in data['source'].keys() :
     entry = data['source'][id]
     meta = {
         'linguistic': linguistic['linguistic']
     }
     title = ''
+    rendered[id] = {}
     if entry['category'] == 'definition' :
         title = 'Definition of %s' % linguistic['linguistic'][id]['noun']
     else :
         title = (linguistic['linguistic'][id]['noun']).capitalize()
-    content = surfaceRealize(entry, meta)
-    output.append({'title': title, 'id': id, 'content': content})
+    rendered[id] = {'title': title, 'id': id}
+    if 'category' in entry.keys() :
+        rendered[id]['content'] = surfaceRealize(entry, meta)
 
 txt = ''
-toc = '<ul class="toc_list">'
-for entry in output :
+for key in rendered.keys() :
+    entry = rendered[key]
     txt += '<h3 id="%s" class="nlg">%s</h3><p>%s</p><a href="#toc_container">Go to top</a><hr>' % (entry['id'], entry['title'], entry['content'])
-    toc += '<li><a href="#%s">%s</a></li>' % (entry['id'], entry['title'])
-toc += '</ul>'
+
+toc = '<ul class="toc_list">%s</ul>' % menu(planner, rendered, linguistic['linguistic'])
 
 html = ''
 with open('templates/index.html', 'r') as file :
